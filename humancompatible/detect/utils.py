d1211d190c92f7ebe8b62c9e6b5e9ad52c726117
GerMati <martige1@fel.cvut.cz>
2025-07-11 14:15:53 +0200

    Add l_inf method to detect

--- humancompatible/detect/utils.py
+++ humancompatible/detect/utils.py
@@ -1,7 +1,9 @@
 import logging
 from typing import Any, List
+from random import randrange
 
 import numpy as np
+import scipy.optimize as optimize
 
 logger = logging.getLogger(__name__)
 
@@ -237,3 +239,29 @@ def report_subgroup_bias(
         # TODO this "=" is not robust to other bins - e.g. continuous ones.
         pretty.append(f"{human_feat} = {human_val}")
     print("Explained rule: " + " AND ".join(pretty))
+
+
+def lin_prog_feas(hist1,hist2,Delta,num_samples=1):
+    """Specifies a number of samples as a fraction of the total
+    histogram bins and checks whether all the sampled bins satisfy
+    |hist1-hist2|<=Delta
+    """
+    rand_lst1 = []
+    rand_lst2 = []
+    for _ in range(0,int(num_samples*(hist1.shape[0]-1))):
+        i = randrange(0,hist1.shape[0]-1)
+        rand_lst1.append(float(hist1[i]))
+        rand_lst2.append(float(hist2[i]))
+    rand_arr1 = np.expand_dims(np.array(rand_lst1),axis=1)
+    rand_arr2 = np.expand_dims(np.array(rand_lst2),axis=1)
+    # We are not interested in the optimization itself, but in the
+    # feasibility of the problem, therefore the coefficient in the
+    # objective function is set to 0 and the only variable (x_0) is
+    # fixed at 1
+    c = 0 ; x0_bounds = (1,1)
+    # Accomodate for the + & - signs of the absolute value in
+    # |r_a1-r_a2|<=Delta
+    A_ub = np.vstack((rand_arr1,-rand_arr1))
+    b_ub = np.vstack((Delta+rand_arr2,Delta-rand_arr2))
+    res = optimize.linprog(c,A_ub,b_ub,bounds=[x0_bounds])
+    return res.status
