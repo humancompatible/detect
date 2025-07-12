95952ca473cd5de886a71da88abdab493a3abced
Illia Kryvoviaz <illiakryvoviaz@gmail.com>
2025-07-13 00:47:09 +0200

    Small addition in __init__.py for detect

--- humancompatible/detect/__init__.py
+++ humancompatible/detect/__init__.py
@@ -1,7 +1,8 @@
-from .detect import detect_bias, detect_bias_csv, detect_bias_two_samples
+from .detect import detect_bias, detect_bias_csv, detect_bias_two_samples, prepare_dataset
 
 __all__ = [
     "detect_bias",
     "detect_bias_csv",
     "detect_bias_two_samples",
+    "prepare_dataset",
 ]
